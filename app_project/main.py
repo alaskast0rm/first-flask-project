1a87dcb0634981e54fb9bdd9bec3d7401d8d16c6
alaskast0rm <montanaof300@mail.ru>
2020-09-16 22:22:00 +0300

    Added db and post detail page

--- app_project/main.py
+++ app_project/main.py
@@ -1,6 +1,9 @@
-from app_project.app import app
+from app_project.app import app, db
+from app_project.posts.blueprint import posts_blue
 import app_project.view
 
+
+app.register_blueprint(posts_blue, url_prefix="/blog")
 
 if __name__ == "__main__":
     app.run()
--- app_project/posts/blueprint.py
+++ app_project/posts/blueprint.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template
 
+from app_project.models import Post
 
-posts = Blueprint("posts", __name__, template_folder="templates")
+posts_blue = Blueprint("posts", __name__, template_folder="templates")
 
 
-@posts.route("/")
+@posts_blue.route("/")
 def index():
-    return render_template("posts/index.html")
+    posts = Post.query.all()
+
+    return render_template("posts/index.html", posts=posts)
+
+
+@posts_blue.route("/<slug>")
+def post_detail(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+
+    return render_template("posts/post_detail.html", post=post)
